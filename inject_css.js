d587c709b5c64c3f20a9e83d668ee6883f04f374
Saul <scommab@gmail.com>
2012-01-04 21:04:30 -0800

    Initial check in

---
+++ inject_css.js
@@ -0,0 +1,7 @@
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel  = 'stylesheet';
+link.type = 'text/css';
+link.href = "http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css";
+link.media = 'all';  
+head.appendChild(link);
