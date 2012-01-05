638e85c33eb0f54168d957233dde63a4f08d9515
Saul <scommab@gmail.com>
2012-01-05 11:33:15 -0800

    Switching back to using 1.4.0 instead of git link

--- inject_css.js
+++ inject_css.js
@@ -3,6 +3,6 @@ var link = document.createElement('link');
 link.rel  = 'stylesheet';
 link.type = 'text/css';
 // Always use the head version
-link.href = "https://raw.github.com/twitter/bootstrap/master/bootstrap.min.css";
+link.href = "http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css"
 link.media = 'all';  
 head.appendChild(link);
