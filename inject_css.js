3a33cdf9d00841c1674967c11c24c4d6e16a24e0
Saul <scommab@gmail.com>
2012-01-04 21:58:41 -0800

    Switching to using the git version of bootstrap.min.css instead of a specific version

--- inject_css.js
+++ inject_css.js
@@ -2,6 +2,7 @@ var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel  = 'stylesheet';
 link.type = 'text/css';
-link.href = "http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css";
+// Always use the head version
+link.href = "https://raw.github.com/twitter/bootstrap/master/bootstrap.min.css";
 link.media = 'all';  
 head.appendChild(link);
